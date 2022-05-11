8523393c839609caf84e4dea9fd7b786e4361d94
Augusto Emmel Licht <augusto.emmel@gmail.com>
2022-05-11 17:21:20 -0300

    conexao inicial

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Navbar />
+    <button>Inscrever Ação V</button>
+  </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components:{
+    Navbar
+  },
+  data() {
+    return {
+      connection: null,
+    };
+  },
+  methods:{
+    sendMessage: function(message){
+      console.log(this.connection)
+      this.connection.send(message)
+    }
+  },
+  created() {
+    console.log("Começando conexão com servidor...");
+    this.connection = new WebSocket("ws://localhost:8080");
+
+    this.connection.onopen = function (event) {
+      console.log(event);
+      console.log("Conectador ao servidor com sucesso!");
+    };
+
+    this.connection.onmessage = function (event) {
+      console.log(event);
+    };
+  },
+};
+</script>
 
 <style>
 #app {
@@ -14,17 +46,7 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+div #app{
+  background: #eee
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+nav.navbar {
+  padding: 30px;
+  background: #eee;
+}
+
+nav.navbar  a{
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav.navbar a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
